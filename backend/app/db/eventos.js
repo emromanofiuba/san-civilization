cb87524343b80b174e6882e5218be2b3df136238
Manuel Camejo <mcamejo@fi.uba.ar>
2026-06-11 21:33:59 -0300

    Todas las entidades tienen su getOne

--- backend/app/db/eventos.js
+++ backend/app/db/eventos.js
@@ -1,7 +1,11 @@
 import { db } from "./pool.js";
 
 export async function getAllEventos() {
-  const res = await db.query("SELECT nombre FROM eventos");
+  const res = await db.query("SELECT * FROM eventos");
   return res.rows;
 }
 
+export async function getOneEvento(id) {
+  const res = await db.query("SELECT * FROM eventos WHERE id = $1", [id]);
+  return res.rows;
+}
