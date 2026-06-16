5d96523bca6adab04055d74ac982030e328737ef
Manuel Camejo <mcamejo@fi.uba.ar>
2026-06-16 19:29:10 -0300

    Primer frontend!

---
+++ frontend/app.js
@@ -0,0 +1,20 @@
+async function unaFuncion(limit, offset) {
+    // const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+    // const response = await fetch(url);
+    // const data = await response.json();
+
+    // const pokemones = data.results;
+
+    // const listaPokemones = document.getElementById("pokemon-list");
+    // pokemones.forEach(pokemon => {
+    //     const unLi = document.createElement("li");
+
+    //     const pokemonLink = document.createElement("a");
+
+    //     pokemonLink.href = pokemon.url;
+    //     pokemonLink.textContent = pokemon.name;
+
+    //     unLi.appendChild(pokemonLink);
+    //     listaPokemones.appendChild(unLi);
+    // });
+}
